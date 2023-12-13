b55071348ae27912084a4dea7c180c5d9304ac18
Nirali <niralipanchal2022@hotmail.com>
2023-12-13 10:03:54 -0500

    add

---
+++ HelloWorldjenkins.java
@@ -0,0 +1,11 @@
+public class HelloWorldjenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to jenkins!!");
+		
+
+	}
+
+}
